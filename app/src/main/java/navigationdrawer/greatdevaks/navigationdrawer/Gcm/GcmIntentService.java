529b30db5b0ecfcfdb3546b853b9f1752e926d97
Aman Priyadarshi <aman.eureka@gmail.com>
2016-02-21 11:51:27 +0530

    Added push notification

--- app/src/main/java/navigationdrawer/greatdevaks/navigationdrawer/Gcm/GcmIntentService.java
+++ app/src/main/java/navigationdrawer/greatdevaks/navigationdrawer/Gcm/GcmIntentService.java
@@ -1,7 +1,10 @@
 package navigationdrawer.greatdevaks.navigationdrawer.Gcm;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -9,11 +12,17 @@ import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import java.util.Random;
+
+import navigationdrawer.greatdevaks.navigationdrawer.MainActivity;
+import navigationdrawer.greatdevaks.navigationdrawer.R;
+
 /**
  * Created by amaneureka on 30-Jan-16.
  */
 public class GcmIntentService  extends IntentService
 {
+    public static int notifyID = 9001;
     NotificationManager mNotificationManager;
     NotificationCompat.Builder builder;
 
@@ -32,11 +41,40 @@ public class GcmIntentService  extends IntentService
         {
             if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType))
             {
-                if(extras.getString("message") != null)
-                    Log.e("message", extras.getString("message"));
+                if(extras.getString("message") != null) {
+                    String _m = extras.getString("message");
+                    Log.d("[gcm]", _m);
+                    sendNotification(_m);
+                }
             }
-            Log.e("[gcm]", "CALLED");
         }
         GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private void sendNotification(String msg) {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        resultIntent.putExtra("msg", msg);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder mNotifyBuilder;
+        NotificationManager mNotificationManager;
+
+        String[] _msgs = msg.split("=");
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                    .setContentTitle(_msgs[0])
+                    .setContentText(_msgs[1])
+                    .setSmallIcon(R.drawable.all_contacts_icon);
+
+        mNotifyBuilder.setContentIntent(resultPendingIntent);
+
+        int defaults = 0;
+        defaults = defaults | Notification.DEFAULT_LIGHTS;
+        defaults = defaults | Notification.DEFAULT_VIBRATE;
+        defaults = defaults | Notification.DEFAULT_SOUND;
+
+        mNotifyBuilder.setAutoCancel(true);
+        mNotificationManager.notify(++notifyID, mNotifyBuilder.build());
     }
 }
